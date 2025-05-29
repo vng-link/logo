7700c6aa99263b2dd624535b4cfbacebd985fe02
vadim.vladyka <vadim.vladyka@yandex.ru>
2025-05-29 20:29:03 +0300

    working main page

---
+++ source/js/modules/sliders/init-intro-slider.js
@@ -0,0 +1,13 @@
+const initIntroSlider = () => {
+  const slider = document.querySelector('[data-slider="intro"]');
+
+  if (!slider) {
+    return;
+  }
+
+  const swiper = new Swiper({
+
+  });
+};
+
+export {initIntroSlider};
