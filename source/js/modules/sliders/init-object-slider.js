2c09f890ec0367c99a3d8278578b19ff0c85920d
vadim.vladyka <vadim.vladyka@yandex.ru>
2025-06-10 16:59:00 +0300

    working

--- source/js/modules/sliders/init-object-slider.js
+++ source/js/modules/sliders/init-object-slider.js
@@ -2,6 +2,8 @@ const initObjectSlider = () => {
 
   const objectSlider = document.querySelector('[data-slider="object-slider"]');
   const objectSubSlider = document.querySelector('[data-slider="object-sub-slider"]');
+  const buttonPrev = document.querySelector('[data-slider="object-slider-button-prev"]');
+  const buttonNext = document.querySelector('[data-slider="object-slider-button-next"]');
 
   if (!objectSlider || !objectSubSlider) return;
 
@@ -10,6 +12,10 @@ const initObjectSlider = () => {
     slidesPerView: 3,
     spaceBetween: 10,
     direction: 'vertical',
+    navigation: {
+      nextEl: buttonNext,
+      prevEl: buttonPrev,
+    },
     breakpoints: {
       0: {
         direction: 'horizontal',
@@ -22,6 +28,10 @@ const initObjectSlider = () => {
   });
 
   const mainObjectSlider = new Swiper(objectSlider, {
+    navigation: {
+      nextEl: buttonNext,
+      prevEl: buttonPrev,
+    },
     thumbs: {
       swiper: mainObjectSubSlider,
     },
