388f2245dd12b7c3e6bd45b4604c502ab3ae1c8c
vadim.vladyka <vadim.vladyka@yandex.ru>
2025-06-27 10:13:54 +0300

    working

--- source/js/modules/modal/modals.js
+++ source/js/modules/modal/modals.js
@@ -13,7 +13,6 @@ export class Modals {
     this._enableScrolling = true;
     this._settingKey = 'default';
 
-    this._productTitle = null;
 
     this._settings = settings;
     this._preventDefault = this._settings[this._settingKey].preventDefault;
@@ -87,10 +86,6 @@ export class Modals {
 
     this._modalName = target.closest('[data-open-modal]').dataset.openModal;
 
-    if (this._modalName === "modal-order-form") {
-      const productTitle = target.closest('[data-open-modal]').querySelector('[data-product-title]').innerHTML;
-      this._productTitle = productTitle;
-    }
 
     if (!this._modalName) {
       return;
@@ -151,10 +146,6 @@ export class Modals {
 
     this._setSettings(modalName);
 
-    if (this._modalName === "modal-order-form") {
-      modal.querySelector('[data-modal-product-title]').innerHTML = this._productTitle;
-      modal.querySelector('[data-modal-product-value]').value = this._productTitle;
-    }
 
     modal.classList.add('is-active');
 
